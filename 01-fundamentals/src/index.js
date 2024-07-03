306db55890ab676d0542135c8bf4123d82d7833e
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-07-03 20:14:34 +0600

    create project and write first component

---
+++ 01-fundamentals/src/index.js
@@ -0,0 +1,11 @@
+
+import Rect from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Greeting(){
+  return <h1>My First Component</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting/>);
