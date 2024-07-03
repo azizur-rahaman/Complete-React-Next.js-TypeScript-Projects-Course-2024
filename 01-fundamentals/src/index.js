e0db0ef0b282bb63e755caa8f6b2edd91aa6b42f
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-07-04 01:31:37 +0600

    complete all of the design

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,11 +1,35 @@
 
-import Rect from 'react'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
+import './index.css';
+import {bookList} from './booklist.js';
+import Book from './Book.js';
 
-function Greeting(){
-  return <h1>My First Component</h1>;
+const author = "Lauren Roberts";
+const title = "Reckless (The Powerless Trilogy)";
+const img = "./images/image.jpg";
+const alt = "Reckless (The Powerless Trilogy) book cover";
+
+
+export const BookList = () => {
+  return (
+    <>
+    <h1>Amazon Best Seller</h1>
+      <section className='bookList'>
+      {
+        bookList.map((book,index)=> {
+          console.log("book",book);
+          console.log("index: ",index);
+          return <Book key={index} {...book} index={index}></Book>
+        })
+      }
+    </section>
+    </>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList/>);
