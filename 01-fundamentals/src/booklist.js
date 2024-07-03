6613c8618f6423ddc2f64347d4af83b887fec303
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-07-04 01:30:52 +0600

    extract booklist

---
+++ 01-fundamentals/src/booklist.js
@@ -0,0 +1,21 @@
+export const bookList = [
+    {
+      author : "Lauren Roberts",
+      title : "Reckless (The Powerless Trilogy)",
+      img : "./images/image.jpg",
+      alt : "Reckless (The Powerless Trilogy) book cover"
+    },
+    {
+      author : "James Clear",
+      title : "Atomic Habits: An Easy & Proven Way to Build Good Habits",
+      img : "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL900_SR900,600_.jpg",
+      alt : "Reckless (The Powerless Trilogy) book cover"
+    }
+    ,
+    {
+        author : "Rebecca Yarros",
+        title : "Iron Flame (The Empyrean, 2)",
+        img: "https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL900_SR900,600_.jpg",
+        alt : "Reckless (The Powerless Trilogy) book cover"
+      }
+];
